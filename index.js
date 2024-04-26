867be93b1ef5d37ee4f10b90923b620d5d689e46
scott <scottysora@gmail.com>
2024-04-26 14:59:24 -0400

    add button handlers

---
+++ index.js
@@ -0,0 +1,45 @@
+window.onload = init;
+
+function init() {
+  const addBtn = document.getElementById("addBtn");
+
+  const subBtn = document.getElementById("subBtn");
+
+  const multBtn = document.getElementById("multBtn");
+
+  const divBtn = document.getElementById("divBtn");
+
+  addBtn.onclick = addClicked;
+
+  subBtn.onclick = subClicked;
+
+  multBtn.onclick = multClicked;
+
+  divBtn.onclick = divClicked;
+}
+
+function addClicked() {
+
+    // 
+
+}
+
+function subClicked() {
+
+   
+  
+  }
+  
+  
+  function multClicked() {
+  
+    
+  
+  }
+  
+  
+  function divClicked() {
+  
+    
+  
+  }
